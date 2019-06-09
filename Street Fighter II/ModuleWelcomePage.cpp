81416c287b0e73cdecd5f78d34ddf498e31379c0
mtorres5254 <47557307+mtorres5254@users.noreply.github.com>
2019-06-09 16:47:43 +0200

    Update ModuleWelcomePage.cpp

--- Street Fighter II/ModuleWelcomePage.cpp
+++ Street Fighter II/ModuleWelcomePage.cpp
@@ -183,7 +183,7 @@ update_status ModuleWelcomePage::Update()
 	}
 	if (App->input->keyboard[SDL_SCANCODE_SPACE] == 1 || App->input->Pad1.button_state[SDL_CONTROLLER_BUTTON_X] == 1) {   // TREURE ABANS D?ENVIAR
 		App->audio->StopMusic(300);
-		App->fade->FadeToBlack(App->welcome_page, App->selectionScene);
+		App->fade->FadeToBlack(App->welcome_page, App->scene_dhalsim);
 	}
 
 
